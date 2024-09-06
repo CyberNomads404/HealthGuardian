7c532edab12b4046f7a8bec50ec1decc33a81737
Thayna Bezerra <arraisthayna@gmail.com>
2024-09-06 20:54:57 -0300

    feat: :sparkles: Migrations, Models e Factories para Heart Rate e Blood Pressure

---
+++ app/Models/HeartRate.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class HeartRate extends Model
+{
+    use HasFactory;
+
+    protected $table = 'heart_rates';
+
+    protected $fillable = ['heart_rate'];
+
+    protected $casts = [
+        'heart_rate' => 'integer',
+    ];
+}
